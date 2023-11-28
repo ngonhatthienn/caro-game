e8da510b46ae35c62723e40aa7a024ba4f5668b8
Nhat Thien <76780335+ngonhatthien120@users.noreply.github.com>
2023-11-28 20:44:09 +0700

    commit

---
+++ history.js
@@ -0,0 +1,96 @@
+fetch('http://localhost:8080/history', {
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/json',
+		// 'Authorization': 'Bearer ' + token // if needed
+	},
+})
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data);
+
+		console.log(data.historyList);
+		console.log(data.totalPages);
+
+		const historyList = data.historyList;
+		let html = '';
+		historyList.forEach((item) => {
+			let result1 = (result2 = class1 = class2 = '');
+			if (item.player1Score > item.player2Score) {
+				result1 = 'WIN';
+				result2 = 'LOSS';
+				class1 = 'B2';
+				class2 = 'B1';
+			} else {
+				result2 = 'WIN';
+				result1 = 'LOSS';
+				class2 = 'B2';
+				class1 = 'B1';
+			}
+			let date = new Date(item.timestamp);
+
+			let options = {
+				weekday: 'short',
+				year: 'numeric',
+				month: 'long',
+				day: 'numeric',
+				hour: '2-digit',
+				minute: '2-digit',
+			};
+
+			let formattedDate = date.toLocaleDateString('en-US', options);
+            
+			html += `
+            <div class="itemMatch">
+            <div class="header-match">
+                <span>Champion Leaguage</span>
+                <span>Success 2vs2 Leaguage</span>
+            </div>
+            <div class="body-match">
+                <div class="time-match">
+                    <span>${formattedDate}</span>
+                    <span>Round 1</span>
+                </div>
+                <div class="result-match">
+                    <div class="home">
+                        <div class="image-team">
+                            <img src="https://play-lh.googleusercontent.com/pnvqAY43TOKv4MR1tQHWdQtxPFp-L0aaxvAX32ftI9UB1BgqOnKOpHP1yM8EkcCQtAGA"
+                                alt="" style="width: 100px;height: 100px;border-radius:50%; object-fit: cover;">
+                        </div>
+                        <div class="info-team">
+                            <div class="name-team">${item.player1}</div>
+                            <div class="result-team">0-2</div>
+                            <div class="number-team"><i class="fa-solid fa-user"></i> <span> 3</span></div>
+                        </div>
+                    </div>
+                    <div class="showMatch">
+                        <h1 style="text-align: center; margin-bottom: 5px;font-size: 40px;">VS</h1>
+                        <p><span class="${class1}">${result1}</span> ${item.player1Score} - ${item.player2Score} <span class="${class2}">${result2}</span></p>
+                    </div>
+                    <div class="guest">
+                        <div class="info-team">
+                            <div class="name-team">${item.player2}</div>
+                            <div class="result-team">0-2</div>
+                            <div class="number-team"> <i class="fa-solid fa-user"></i> 3</div>
+                        </div>
+                        <div class="image-team">
+                            <img src="https://play-lh.googleusercontent.com/pnvqAY43TOKv4MR1tQHWdQtxPFp-L0aaxvAX32ftI9UB1BgqOnKOpHP1yM8EkcCQtAGA"
+                                alt="" style="width: 100px;height: 100px;border-radius:50%; object-fit: cover;">
+                        </div>
+                    </div>
+                </div>
+                </div>
+             </div>
+			`;
+		});
+        document.getElementsByClassName('content')[0].style.display = 'grid';
+		document.getElementsByClassName('listMatch')[0].innerHTML = html;
+        document.getElementsByClassName('no-data')[0].style.display = 'none';
+
+	})
+	.catch((error) => {
+		console.error('Error:', error);
+        document.getElementsByClassName('no-data')[0].style.display = 'grid';
+        document.getElementsByClassName('content')[0].style.display = 'none';
+
+	});
---
+++ script.js
@@ -0,0 +1,132 @@
+var navbar = document.getElementById('navbar');
+var signupBtn = document.getElementById('signup-btn');
+var loginBtn = document.getElementById('login-btn');
+
+var cover = document.getElementById('cover');
+var popup = document.getElementById('popup');
+var loginForm = document.getElementById('login-form');
+var regForm = document.getElementById('reg-form');
+
+var reg = document.getElementById('reg');
+var login = document.getElementById('login');
+
+// signup
+var username = document.getElementById('username');
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+
+// login
+var usernameLogin = document.getElementById('user');
+var passwordLogin = document.getElementById('pass');
+
+var cancel = document.getElementById('cancel');
+
+signupBtn.addEventListener('click', function () {
+	// Show the popup
+	// alert("Hello! I am an alert box!!");
+	cover.style.display = 'block';
+	navbar.style.display = 'none';
+
+	popup.style.display = 'block';
+	// Hide the navbar
+	// Blur the background
+	cover.style.filter = 'blur(5px)';
+});
+loginBtn.addEventListener('click', function () {
+	// Show the popup
+	cover.style.display = 'block';
+	navbar.style.display = 'none';
+	regForm.style.display = 'none';
+
+	popup.style.display = 'block';
+	// Hide the navbar
+	// Blur the background
+	cover.style.filter = 'blur(5px)';
+});
+
+reg.addEventListener('click', function (event) {
+	// Prevent the default behavior of the form submission
+	event.preventDefault();
+	// Get the input values
+	var usernameValue = username.value;
+	var passwordValue = password.value;
+	var emailValue = email.value;
+	console.log(usernameValue);
+	// Validate the input values
+	if (usernameValue && passwordValue && emailValue) {
+		// Send the login data to a server using the fetch method
+		fetch('http://localhost:8080/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username: usernameValue,
+				email: emailValue,
+				password: passwordValue,
+			}),
+		})
+			.then(function (response) {
+				// Handle the response
+				if (response.ok) {
+					// Login successful
+					alert('Register successful');
+					window.location.href = './history.html';
+				} else {
+					// Login failed
+					alert('Register failed');
+				}
+			})
+			.catch(function (error) {
+				// Handle the error
+				alert('Error: ' + error);
+			});
+	} else {
+		// Input values are empty
+		alert('Please enter your username and password');
+	}
+});
+
+login.addEventListener('click', function (event) {
+	// Prevent the default behavior of the form submission
+	event.preventDefault();
+	// Get the input values
+
+	var usernameValue = usernameLogin.value;
+	var passwordValue = passwordLogin.value;
+	console.log(usernameValue);
+	console.log(passwordValue);
+
+	// Validate the input values
+	if (usernameValue && passwordValue) {
+		// Send the login data to a server using the fetch method
+		fetch('http://localhost:8080/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username: usernameValue,
+				password: passwordValue,
+			}),
+		})
+			.then(function (response) {
+				// Handle the response
+				if (response.ok) {
+					// Login successful
+					alert('Login successful');
+					window.location.href = './history.html';
+				} else {
+					// Login failed
+					alert('Login failed');
+				}
+			})
+			.catch(function (error) {
+				// Handle the error
+				alert('Error: ' + error);
+			});
+	} else {
+		// Input values are empty
+		alert('Please enter your username and password');
+	}
+});
